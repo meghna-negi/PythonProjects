18e834d948d29ddf4bb4aebd3f63c801f9b9fd79
Meghna Negi <meghnanegi510@gmail.com>
2024-11-28 01:13:03 +0100

    Modifying code to validate the email address and length of the password for the WTForms exercise for Day 61

--- HTMLWTFormsIntro_Day61/main.py
+++ HTMLWTFormsIntro_Day61/main.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired,Email,Length
 import dotenv
 
 #Getting the environment variables
 config = dotenv.dotenv_values("./.env")
 
-#Creating class for creating For,
+#Creating class for creating Form
+#Validations should be done to check if the email address is valid or not and if password is atleat 8 characters long
 class MyForm(FlaskForm):
-    email = StringField(label = 'Email')
-    password = PasswordField(label = 'Password')
+    email = StringField(label = 'Email', validators=[DataRequired(),Email('Invalid email address.')])
+    password = PasswordField(label = 'Password', validators=[DataRequired(), Length(min=8,message='Field must be at least 8 characters long.')])
     submit = SubmitField(label = 'Log In')
 
 app = Flask(__name__)
@@ -24,6 +26,7 @@ def home():
 @app.route("/login",methods=['GET','POST'])
 def login():
     login_form = MyForm()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 if __name__ == '__main__':
